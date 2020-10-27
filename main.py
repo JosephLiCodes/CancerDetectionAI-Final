51db2ced176f9efa49a297992cb65c4dec074f9f
JosephLiCodes <jlicodes@users.noreply.github.com>
2020-10-27 11:28:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ if(trainF.lower() == "y" or trainF.lower() == "yes" ):
     #print(cancer.target_names)
 
     x = cancer.data
-    y = cancer.target
+    y = cancer.target       
 
     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2)
     classes = ['malignant', 'benign']
